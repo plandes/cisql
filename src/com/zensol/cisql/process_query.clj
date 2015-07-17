ead2c5b2b1462198f6effc4267d1ddc63142959d
Paul Landes <landes@mailc.net>
2015-07-17 05:28:37 -0500

    forgot to add file

---
+++ src/com/zensol/cisql/process_query.clj
@@ -0,0 +1,86 @@
+(ns com.zensol.cisql.process-query
+  (:require [clojure.tools.logging :as log]
+            [clojure.string :as str])
+  (:import (java.io BufferedReader InputStreamReader StringReader))
+  (:require [com.zensol.cisql.conf :as c]
+            [com.zensol.cisql.db-access :as db]))
+
+(def ^:dynamic *std-in* nil)
+
+(def ^{:private true
+       :dynamic true}
+  *query* nil)
+
+(defmacro with-query [& body]
+  `(.append *query* (with-out-str ~@body)))
+
+(defn- add-line [line]
+  (letfn [(has-eol [key]
+            (let [conform (str/lower-case (str/trim line))]
+              (.endsWith conform (c/config key))))
+          (find-keyword [key]
+            (str/trim
+             (subs line 0 (- (count line)
+                             (count (c/config key))))))]
+    (cond (has-eol :line-terminator)
+          (do (with-query
+                (print (find-keyword :line-terminator)))
+              :end-query)
+
+          (has-eol :end-directive)
+          (do (with-query
+                (print (find-keyword :end-directive)))
+              :end-session)
+
+          :default
+          (do (with-query
+                (println line))
+              :continue))))
+
+(defn- read-query []
+  (binding [*query* (StringBuilder.)]
+    (let [lines-left (atom 20)
+          directive (atom nil)]
+      (letfn [(end [dir]
+                (log/debugf "query: %s" *query*)
+                (reset! lines-left 0)
+                (reset! directive dir))]
+        (while (> @lines-left 0)
+   (log/tracef "lines left: %d" @lines-left)
+   (let [user-input (.readLine *std-in*)]
+     (log/debugf "line: %s" user-input)
+     (if-not user-input
+       (do
+         (log/debugf "read EOF from input")
+         (end :end-file))
+       (let [dir (add-line user-input)]
+         (log/tracef "dir: %s" dir)
+         (log/tracef "query so far: %s" *query*)
+         (case dir
+           :end-query (end dir)
+           :end-session (end dir)
+           :continue (log/tracef "continue...")
+           :default (throw (IllegalStateException.
+                            (str "unknown directive: " dir)))))))
+   (swap! lines-left dec)))
+      {:query (str/trim (.toString *query*))
+       :directive @directive})))
+
+(defn process-queries [dir-fns]
+  (let [prompt-fn (get dir-fns :prompt-for-input)]
+    (prompt-fn)
+    (loop [query-data (read-query)]
+      (log/tracef "query data: %s" query-data)
+      (log/debugf "query: <%s>" (:query query-data))
+      (if-let [dir-fn (get dir-fns (:directive query-data))]
+        (dir-fn query-data)
+        (throw (IllegalArgumentException.
+                (str "no mapping for directive: "
+                     (:directive query-data)))))
+      (when (= :end-query (:directive query-data))
+        (prompt-fn)
+        (recur (read-query))))))
+
+(defn process-query-string [query-string dir-fns]
+  (binding [*std-in* (BufferedReader. (StringReader. query-string))]
+    (process-queries dir-fns)))
