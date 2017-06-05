546c94eaf4493048c48456b9f4359f5beed2557c
Paul Landes <landes@mailc.net>
2017-06-05 10:01:48 -0500

    dep

--- project.clj
+++ project.clj
@@ -44,7 +44,7 @@
                  [org.apache.logging.log4j/log4j-jcl "2.7"]
 
                  ;; command line
-                 [com.zensols.tools/actioncli "snapshot"]]
+                 [com.zensols.tools/actioncli "0.0.16"]]
   :pom-plugins [[org.codehaus.mojo/appassembler-maven-plugin "1.6"
                  {:configuration ([:programs
                                    [:program
